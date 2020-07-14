419f11be3ceec7cc73bfa3d744aa143e71180498
Mohammad Hosein Mazinani <mhmazinani1994@gmail.com>
2020-07-14 13:49:18 +0430

    Fix userinfo missing fields

--- Authin.Api.Sdk/Model/UserInfoResponse.cs
+++ Authin.Api.Sdk/Model/UserInfoResponse.cs
@@ -4,13 +4,13 @@ namespace Authin.Api.Sdk.Model
 {
     public class UserInfoResponse
     {
-        [JsonProperty("id")] public string Id { get; set; }
+        [JsonProperty("sub")] public string Sub { get; set; }
         [JsonProperty("profile")] public string Profile { get; set; }
         [JsonProperty("email")] public string Email { get; set; }
-        [JsonProperty("phoneNumber")] public string PhoneNumber { get; set; }
-        [JsonProperty("username")] public string Username { get; set; }
+        [JsonProperty("phone_number")] public string PhoneNumber { get; set; }
+        [JsonProperty("preferred_name")] public string PreferredUsername { get; set; }
         [JsonProperty("name")] public string Name { get; set; }
-        [JsonProperty("firstName")] public string FirstName { get; set; }
-        [JsonProperty("lastName")] public string LastName { get; set; }
+        [JsonProperty("given_name")] public string GivenName { get; set; }
+        [JsonProperty("family_name")] public string FamilyName { get; set; }
     }
 }
