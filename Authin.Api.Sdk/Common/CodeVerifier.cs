975ecaa8fd65951f8bede25d084193db84756659
Mohammad Hosein Mazinani <mhmazinani1994@gmail.com>
2021-02-22 16:33:52 +0330

    Add support for PKCE

---
+++ Authin.Api.Sdk/Common/CodeVerifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Authin.Api.Sdk.Common
+{
+    public class CodeVerifier
+    {
+        public static string NewCodeVerifier()
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var tokenData = new byte[128];
+                rng.GetBytes(tokenData);
+                return tokenData.ToBase64UrlSafeNoPadding();
+            }
+        }
+    }
+}
---
+++ Authin.Api.Sdk/Common/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Authin.Api.Sdk.Common
+{
+    public static class Extensions
+    {
+        public static byte[] ToSha256(this string value)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Encoding.UTF8.GetBytes(value));
+            }
+        }
+
+        public static string ToBase64UrlSafeNoPadding(this byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes)
+                .Replace('+', '-')
+                .Replace('/', '_')
+                .Replace("=", "");
+        }
+    }
+}
