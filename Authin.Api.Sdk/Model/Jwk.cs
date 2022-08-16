da3c4c1d400ff9fe0760c2ec85d6551069d25554
Mohammad Hossein Mazinani <mhmazinani1994@gmail.com>
2022-08-16 09:15:37 +0430

    fix: jwks deserilaization

--- Authin.Api.Sdk/Model/Jwk.cs
+++ Authin.Api.Sdk/Model/Jwk.cs
@@ -1,12 +1,11 @@
-﻿using System.Runtime.Serialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace Authin.Api.Sdk.Model
 {
     public class Jwk
     {
-        [JsonProperty("kty")] public KeyType KeyType { get; set; }
-        [JsonProperty("use")] public PublicKeyUse PublicKeyUse { get; set; }
+        [JsonProperty("kty")] public string KeyType { get; set; }
+        [JsonProperty("use")] public string PublicKeyUse { get; set; }
         [JsonProperty("key_ops")] public string KeyOperations { get; set; }
         [JsonProperty("alg")] public string Algorithm { get; set; }
         [JsonProperty("kid")] public string KeyId { get; set; }
@@ -14,36 +13,5 @@ namespace Authin.Api.Sdk.Model
         [JsonProperty("x5c")] public string X509CertificateChain { get; set; }
         [JsonProperty("x5t")] public string X509CertificateSha1Thumbprint { get; set; }
         [JsonProperty("x5t#s256")] public string X509CertificateSha256Thumbprint { get; set; }
-    }
-
-    public enum KeyType
-    {
-        [EnumMember(Value = "RSA")] Rsa,
-        [EnumMember(Value = "EC")] Ec
-    }
-
-    public enum PublicKeyUse
-    {
-        [EnumMember(Value = "sig")] Signature,
-        [EnumMember(Value = "enc")] Encryption
-    }
-
-    public enum KeyOperations
-    {
-        [EnumMember(Value = "sign")] Sign,
-        [EnumMember(Value = "verify")] Verify,
-        [EnumMember(Value = "encrypt")] Encrypt,
-        [EnumMember(Value = "decrypt")] Decrypt,
-        [EnumMember(Value = "wrapkey")] WrapKey,
-        [EnumMember(Value = "unwrapkey")] UnwrapKey,
-        [EnumMember(Value = "derivekey")] DeriveKey,
-        [EnumMember(Value = "derivebits")] DeriveBits
-    }
-
-    public enum Jwa
-    {
-        [EnumMember(Value = "HS256")] Hs256,
-        [EnumMember(Value = "RS256")] Rs256,
-        [EnumMember(Value = "ES256")] Es256
     }
 }
