8c2439f02aacde8afd4df362e31350d42ad0a3de
mostmand <mostmand.mh@gmail.com>
2024-01-13 13:42:12 +0330

    Move package and assembly details to csproj

--- Authin.Api.Sdk/Properties/AssemblyInfo.cs
+++ Authin.Api.Sdk/Properties/AssemblyInfo.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6982a1d1-9d98-4455-864e-1759ec8d5d32")]
